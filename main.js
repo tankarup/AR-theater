63985001969552fed39be39f60f627bdd50aaa27
tankarup <69491808+tankarup@users.noreply.github.com>
2021-04-10 07:00:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ body.addEventListener('touchend', function(event) {
 let simulatingCoords = false;
 body.addEventListener('click', function(event) {
     simulatingCoords = !simulatingCoords;
+    const camera = document.getElementById('camera');
     //document.getElementById('camera').setAttribute('gps-camera', simulatingCoords ? `simulateLatitude:35.65261 ; simulateLongitude:139.79216;` : '');
-    alert(simulatingCoords);
+    alert(camera);
 }, false);
