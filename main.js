00f74911ab971524b62de404fae530e8e81860fe
tankarup <69491808+tankarup@users.noreply.github.com>
2021-04-09 20:40:46 +0900

    touch event

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-
+let onScreenPress = false;
 const body = document.getElementsByTagName('body')[0];
 console.log(body);
 body.addEventListener('touchstart', function(event) {
-    alert('touchstart');
+    document.getElementById('camera').setAttribute('gps-camera', `simulateLatitude:35.65261 ; simulateLongitude:139.79216;`);
+    //alert('touchstart');
     }, false);
 body.addEventListener('touchend', function(event) {
-    alert('touchend');
+    document.getElementById('camera').setAttribute('gps-camera', '');
+    //alert('touchend');
     }, false);
 body.addEventListener('click', function(event) {
     alert('click');
